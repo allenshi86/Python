d10e5aad8c51c548c653a3c537898b81fc5a4af3
allenshi86 <zhenning.shi@163.com>
2021-02-25 11:11:06 +0800

    traverse_file.py

---
+++ traverse_file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+
+def convert_encoding(file,from_encoding='gbk',to_encoding='utf8'):
+    try:
+        with open(file,'r',encoding=from_encoding) as f:
+            filedata = f.readlines()
+        with open(file,'w',encoding=to_encoding) as f:
+            f.writelines(filedata)
+    except Exception as e:
+        print(e)
+
+
+def listfile(path):
+    if os.path.isdir(path):
+        files = os.listdir(path)
+        for filename in files:
+            filepath = os.path.join(path,filename)
+            if os.path.isdir(filepath):
+               listfile(filepath)
+            else:
+                if filename.endswith('.sh'):
+                    print(filepath)
+                    convert_encoding(filepath)
+
+    else:
+        print("%s is not a filepath!" %path)
+
+listfile('/Users/momo/163')
